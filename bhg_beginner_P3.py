f16b1d599b20270fdd4c3be0f3c2490f9e8f8104
Bogdan Hodorog <bogdan.hodorog@3pillarglobal.com>
2014-02-24 10:54:18 +0200

    Nicer review?

--- bhg_beginner_P3.py
+++ bhg_beginner_P3.py
@@ -1,36 +1,36 @@
-# -*- coding: utf-8 -*-
-"""
-Given a file containing a list of dictionaries implement a sorting algorithm (of your choosing)
-that will sort the list based on the dictionary keys.
-The dictionary keys will contain alphabetic characters while the values will be integers
-"""
+# -*- coding: utf-8 -*- #
+""" #
+Given a file containing a list of dictionaries implement a sorting algorithm (of your choosing) #
+that will sort the list based on the dictionary keys. #
+The dictionary keys will contain alphabetic characters while the values will be integers #
+""" #
 
-def read_dicts(file_path):
-    result = []
-    with open(file_path,'r') as f:
-        for line in f:
-            d = {}
-            vals = line.strip().split(' ')
-            for i in range(0,len(vals),2):
-                d[vals[i]] = vals[i+1]
-            result.append(d)
-            #result.append(collections.OrderedDict(sorted(d.iteritems())))
-    return result
-
-
-def sort_dicts(dicts):
-    def _compare(d1, d2):
-       for k1 in sorted(d1.iterkeys()):
-           if k1 in d2:
-               return int(d1[k1]) - int(d2[k1])
-       return 0
+def read_dicts(file_path): #
+    result = [] #
+    with open(file_path,'r') as f: #
+        for line in f: #
+            d = {} #
+            vals = line.strip().split(' ') #
+            for i in range(0,len(vals),2): #
+                d[vals[i]] = vals[i+1] #
+            result.append(d) #
+            #result.append(collections.OrderedDict(sorted(d.iteritems()))) #
+    return result #
 
 
-    return sorted(dicts, cmp=_compare)
+def sort_dicts(dicts): #
+    def _compare(d1, d2): #
+       for k1 in sorted(d1.iterkeys()): #
+           if k1 in d2: #
+               return int(d1[k1]) - int(d2[k1]) #
+       return 0 #
 
-print '#'*40
-dicts = read_dicts('dicts_file.txt')
-print dicts
-print '-'*40
-print sort_dicts(dicts)
-print '#'*40
+
+    return sorted(dicts, cmp=_compare) #
+
+print '#'*40 #
+dicts = read_dicts('dicts_file.txt') #
+print dicts #
+print '-'*40 #
+print sort_dicts(dicts) #
+print '#'*40 #
--- bhg_intermediate_P2.py
+++ bhg_intermediate_P2.py
@@ -1,127 +1,127 @@
-# -*- coding: utf-8 -*-
-"""
-Create an automated card dealer for a Texas Hold’em application.
-It should be able to handle Deck objects, consisting of Cards.
-Cards can be added or removed from Decks, and Decks can be shuffled and sorted.
-When dealing cards, each Player receives a Hand consisting of 2 Cards.
-After all cards are dealt, the Dealer should draw the table Hand of 5 Cards
-"""
-from random import shuffle as rshuffle
+# -*- coding: utf-8 -*- #
+""" #
+Create an automated card dealer for a Texas Hold’em application. #
+It should be able to handle Deck objects, consisting of Cards. #
+Cards can be added or removed from Decks, and Decks can be shuffled and sorted. #
+When dealing cards, each Player receives a Hand consisting of 2 Cards. #
+After all cards are dealt, the Dealer should draw the table Hand of 5 Cards #
+""" #
+from random import shuffle as rshuffle #
 
-class Card(object):
+class Card(object): #
 
-    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    colors = ['clubs', 'diamonds', 'hearts', 'spades']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] #
+    colors = ['clubs', 'diamonds', 'hearts', 'spades'] #
 
-    def __init__(self, value, color):
-        self._value = value
-        self._color = color
+    def __init__(self, value, color): #
+        self._value = value #
+        self._color = color #
 
-    def get_value(self):
-        return self._value
+    def get_value(self): #
+        return self._value #
 
-    def get_color(self):
-        return self._color
+    def get_color(self): #
+        return self._color #
 
-    def get_item(self):
-        return (self._color, self._value)
+    def get_item(self): #
+        return (self._color, self._value) #
 
-    def __str__(self):
-        return '({0:>2},{1:>8})'.format(self._value, self._color)
+    def __str__(self): #
+        return '({0:>2},{1:>8})'.format(self._value, self._color) #
 
-class Deck(object):
+class Deck(object): #
 
-    def __init__(self):
-        self._deck = []
+    def __init__(self): #
+        self._deck = [] #
 
-    def add_card(self, card):
-        self._deck.append(card)
+    def add_card(self, card): #
+        self._deck.append(card) #
 
-    def extend_deck(self, deck):
-        self._deck.extend(deck.get_cards())
+    def extend_deck(self, deck): #
+        self._deck.extend(deck.get_cards()) #
 
-    def get_cards(self):
-        return self._deck
+    def get_cards(self): #
+        return self._deck #
 
-    def pop_card(self):
-        return self._deck.pop()
+    def pop_card(self): #
+        return self._deck.pop() #
 
-    def remove_card(self, card):
-        self._deck.remove(card)
+    def remove_card(self, card): #
+        self._deck.remove(card) #
 
-    def shuffle(self):
-        rshuffle(self._deck)
+    def shuffle(self): #
+        rshuffle(self._deck) #
 
-    def sort(self):
-        self._deck.sort(key=lambda x:x.get_value())
+    def sort(self): #
+        self._deck.sort(key=lambda x:x.get_value()) #
 
-    @classmethod
-    def build_deck(cls):
-        d = Deck()
-        for color in Card.colors:
-            for value in Card.values:
-                d.add_card(Card(value, color))
-        #d.sort()
-        return d;
+    @classmethod #
+    def build_deck(cls): #
+        d = Deck() #
+        for color in Card.colors: #
+            for value in Card.values: #
+                d.add_card(Card(value, color)) #
+        #d.sort() #
+        return d; #
 
-    def __str__(self):
-        result = '['
-        for card in self._deck:
-            result+=card.__str__()+'\n'
-        result+=']'
-        return result
+    def __str__(self): #
+        result = '[' #
+        for card in self._deck: #
+            result+=card.__str__()+'\n' #
+        result+=']' #
+        return result #
 
 
-class Player(object):
+class Player(object): #
 
-    def __init__(self, name):
-        self._name = name
-        self._cards = []
+    def __init__(self, name): #
+        self._name = name #
+        self._cards = [] #
 
-    def get_name(self):
-        return self._name
+    def get_name(self): #
+        return self._name #
 
-    def set_cards(self, cards):
-        self._cards = cards
+    def set_cards(self, cards): #
+        self._cards = cards #
 
-    def get_cards(self):
-        return self._cards
+    def get_cards(self): #
+        return self._cards #
 
-    def __str__(self):
-        result = '['
-        for card in self._cards:
-            result+=card.__str__()+','
-        result+=']'
-        return '{0:20} | {1}'.format(self._name, result)
+    def __str__(self): #
+        result = '[' #
+        for card in self._cards: #
+            result+=card.__str__()+',' #
+        result+=']' #
+        return '{0:20} | {1}'.format(self._name, result) #
 
-class Dealer(Player):
-    _deck = Deck() #the large deck
+class Dealer(Player): #
+    _deck = Deck() #the large deck #
 
-    def __init__(self, name, total_decks):
-        super(Dealer, self).__init__(name)
-        for i in range(total_decks):
-            d = Deck.build_deck()
-            Dealer._deck.extend_deck(d)
-        #print Dealer._deck
+    def __init__(self, name, total_decks): #
+        super(Dealer, self).__init__(name) #
+        for i in range(total_decks): #
+            d = Deck.build_deck() #
+            Dealer._deck.extend_deck(d) #
+        #print Dealer._deck #
 
-    def deal(self, players):
-        Dealer._deck.shuffle()
-        for player in players:
-            cards = []
-            for i in range(2):
-                cards.append(Dealer._deck.pop_card())
-            player.set_cards(cards)
-        cards = []
-        for i in range(5):
-            cards.append(Dealer._deck.pop_card())
-        self.set_cards(cards)
+    def deal(self, players): #
+        Dealer._deck.shuffle() #
+        for player in players: #
+            cards = [] #
+            for i in range(2): #
+                cards.append(Dealer._deck.pop_card()) #
+            player.set_cards(cards) #
+        cards = [] #
+        for i in range(5): #
+            cards.append(Dealer._deck.pop_card()) #
+        self.set_cards(cards) #
 
-players = [Player('Paul'), Player('Zoli'), Player('Bogdan'), Player('Radu'), Player('Marcom')]
-dealer = Dealer('Za Dealer', 3)
-dealer.deal(players)
-print '-'*40
-print dealer
-print '-'*40
-for player in players:
-    print player
-print '-'*40
+players = [Player('Paul'), Player('Zoli'), Player('Bogdan'), Player('Radu'), Player('Marcom')] #
+dealer = Dealer('Za Dealer', 3) #
+dealer.deal(players) #
+print '-'*40 #
+print dealer #
+print '-'*40 #
+for player in players: #
+    print player #
+print '-'*40 #
